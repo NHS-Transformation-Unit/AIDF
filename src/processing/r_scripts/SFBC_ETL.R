45212d68091b8bd11e243c5601879c5157c6244f
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2023-11-22 23:22:03 +0000

    Creation of SFBC ETL script development.

---
+++ src/processing/r_scripts/SFBC_ETL.R
@@ -0,0 +1,12 @@
+# Script for ETL of SFBC and creation of collated data source for geospatial mapping
+# Elliot Royle - 22/11/2023
+
+#Packages for script
+
+library(tidyverse)
+library(readxl)
+library(writexl)
+library(ggridges)
+
+# Data Extraction
+
