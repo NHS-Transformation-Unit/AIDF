b9fd4ebdd1414cdfdfdc13e70f0d42a4c940e2f1
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2023-11-29 12:02:37 +0000

    Creation of a `SFBC_Pipeline.R` scripts for the purposes of running all SFBC scripts.

---
+++ src/processing/r_scripts/SFBC_Pipeline.R
@@ -0,0 +1,7 @@
+# Pipeline script for running all of the SFBC ETL scripts
+
+source("C:/Users/elliot.royle/OneDrive - Midlands and Lancashire CSU/Git/AIDF/src/processing/r_scripts/SFBC_Packages.R")
+source("C:/Users/elliot.royle/OneDrive - Midlands and Lancashire CSU/Git/AIDF/src/processing/r_scripts/SFBC_Load.R")
+source("C:/Users/elliot.royle/OneDrive - Midlands and Lancashire CSU/Git/AIDF/src/processing/r_scripts/SFBC_ETL.R")
+source("C:/Users/elliot.royle/OneDrive - Midlands and Lancashire CSU/Git/AIDF/src/processing/r_scripts/SFBC_Clean.R")
+source("C:/Users/elliot.royle/OneDrive - Midlands and Lancashire CSU/Git/AIDF/src/processing/r_scripts/SFBC_Join.R")
