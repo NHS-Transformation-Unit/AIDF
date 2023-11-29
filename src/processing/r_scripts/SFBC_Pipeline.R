e5ff598eb5e0c232980b5c5c876596e382bf40d2
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2023-11-29 16:13:14 +0000

    Changed paths to be relative.

--- src/processing/r_scripts/SFBC_Pipeline.R
+++ src/processing/r_scripts/SFBC_Pipeline.R
@@ -1,7 +1,7 @@
 # Pipeline script for running all of the SFBC ETL scripts
 
-source("C:/Users/elliot.royle/OneDrive - Midlands and Lancashire CSU/Git/AIDF/src/processing/r_scripts/SFBC_Packages.R")
-source("C:/Users/elliot.royle/OneDrive - Midlands and Lancashire CSU/Git/AIDF/src/processing/r_scripts/SFBC_Load.R")
-source("C:/Users/elliot.royle/OneDrive - Midlands and Lancashire CSU/Git/AIDF/src/processing/r_scripts/SFBC_ETL.R")
-source("C:/Users/elliot.royle/OneDrive - Midlands and Lancashire CSU/Git/AIDF/src/processing/r_scripts/SFBC_Clean.R")
-source("C:/Users/elliot.royle/OneDrive - Midlands and Lancashire CSU/Git/AIDF/src/processing/r_scripts/SFBC_Join.R")
+source("src/processing/r_scripts/SFBC_Packages.R")
+source("src/processing/r_scripts/SFBC_Load.R")
+source("src/processing/r_scripts/SFBC_ETL.R")
+source("src/processing/r_scripts/SFBC_Clean.R")
+source("src/processing/r_scripts/SFBC_Join.R")
