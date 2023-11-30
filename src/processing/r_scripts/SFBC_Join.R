fba80f83b395a414cfef8a6acee30240b7382205
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2023-11-30 13:07:47 +0000

    Changes made to the `SFBC_Join.R` script to reflect the new NCDR data source being used.

--- src/processing/r_scripts/SFBC_Join.R
+++ src/processing/r_scripts/SFBC_Join.R
@@ -4,7 +4,7 @@
 # Preparing the different files to be joined to
 
 AIDF_Summary_File_Path <- paste0(here(),"/data/TU Summaries/AIDF_Network_Summary.xlsx") 
-NHS_Trust_Location_File_Path <- paste0(here(),"/data/Geospatial/NHSTrust_Location.xlsx")
+NHS_Trust_Location_File_Path <- paste0(here(),"/data/Geospatial/NCDR_Reference_Locations.xlsx")
 
 AIDF_Ref_Raw <- read_excel(AIDF_Summary_File_Path)
 NHS_Trust_Lookup <- read_excel(NHS_Trust_Location_File_Path)
@@ -12,17 +12,10 @@ NHS_Trust_Lookup <- read_excel(NHS_Trust_Location_File_Path)
 
 # Cleaning the NHS Trust Lookup
 
-capitalise_NHS <- function(x) {
-  gsub("\\bNHS\\b", "NHS", x, ignore.case = TRUE)
-}
-
 NHS_Trust_Lookup <- NHS_Trust_Lookup %>%
-  mutate(Name = str_to_title(Name)) %>%
-  select(c(2, 1, 10, 11, 12))
+  select(c(1, 11, 15, 16))
 
-NHS_Trust_Lookup$Name <- capitalise_NHS(NHS_Trust_Lookup$Name)
-
-names(NHS_Trust_Lookup)[names(NHS_Trust_Lookup) == "Name"] <- "Trust"
+names(NHS_Trust_Lookup)[names(NHS_Trust_Lookup) == "ODS_Code"] <- "Org code"
 
 # Cleaning the AIDF Summary
 
@@ -38,7 +31,7 @@ AIDF_Ref_Proc <- AIDF_Ref_Proc %>%
 
 AIDF_Expanded <- left_join(AIDF_Proc, AIDF_Ref_Proc, by = "SFBC_ID")
 
-AIDF_Geo_Final <- left_join(AIDF_Expanded, NHS_Trust_Lookup, by = "Trust") # This results in 24 missing locations/organisation codes due to the lookup being out of date
+AIDF_Geo_Final <- left_join(AIDF_Expanded, NHS_Trust_Lookup, by = "Org code") # This results in 24 missing locations/organisation codes due to the lookup being out of date
 
 
 
