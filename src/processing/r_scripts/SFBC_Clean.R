c9c973947932f4dad44357f01acf51a388c8c609
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2023-11-30 13:07:17 +0000

    Changes made to the `SFBC_Clean.R` to manually import org codes for later join.

--- src/processing/r_scripts/SFBC_Clean.R
+++ src/processing/r_scripts/SFBC_Clean.R
@@ -39,18 +39,53 @@ SW1_df <- AIDF_df %>%
 # Cleaning C&M
 
 CM_Row <- 8
-CM_Column <- "Trust"
 CM_Value <- "Mersey and West Lancashire Teaching Hospitals NHS Trust"
 
-NWCM_df[CM_Row, CM_Column] <- CM_Value
+CM_Org <- c("REN",
+            "RJR",
+            "RJN",
+            "RBT",
+            "REM",
+            "REP",
+            "RBQ",
+            "RBN",
+            "RWW",
+            "RBL")
+
+NWCM_df <- NWCM_df %>% 
+  mutate(`Org code` = CM_Org,
+         Trust = ifelse(row_number() == CM_Row, CM_Value, Trust))
+
+# Cleaning E1
+
+E1_Org <- c("RGM",
+            "RGP",
+            "RCX",
+            "RGR",
+            "RGN",
+            "RDE",
+            "RM1",
+            "RGT")
+
+E1_df <- E1_df %>% 
+  mutate(`Org code` = E1_Org)
 
 # Cleaning EM1
 
 EM1_Row <- 7
-EM1_Column <- "Trust"
 EM1_Value <- "University Hospitals of Leicester NHS Trust"
 
-EM1_df[EM1_Row, EM1_Column] <- EM1_Value
+EM1_Org <- c("RFS",
+             "RNQ",
+             "RP1",
+             "RX1",
+             "RK5",
+             "RP7",
+             "RWE")
+
+EM1_df <- EM1_df %>% 
+  mutate(`Org code` = EM1_Org,
+         Trust = ifelse(row_number() == EM1_Row, EM1_Value, Trust))
 
 # Cleaning GM
 
@@ -63,34 +98,59 @@ GM_Trusts <- c("Bolton NHS Foundation Trust",
                "Wrightington, Wigan and Leigh NHS Foundation Trust", 
                "Blank")
 
+GM_Org <- c("RMC",
+            "R0A",
+            "RM3",
+            "RMP",
+            "RBV",
+            "RWJ",
+            "RRF",
+            "Blank")
+
 GM_Remove <- 8
 
 NWGM_df <- NWGM_df %>% 
-  mutate(Trust = GM_Trusts) %>%
-  mutate(Region = "North West") %>%
-  mutate(`Network Name 1` = "Greater Manchester") %>%
-  mutate(`Network Name 2` = "North West 2") %>%
+  mutate(Trust = GM_Trusts,
+         Region = "North West",
+         `Network Name 1` = "Greater Manchester",
+         `Network Name 2` = "North West 2",
+         `Org code` = GM_Org,) %>%
   filter(row_number() != GM_Remove)
 
 # Cleaning HNY
 
 HNY_Row <- c(1, 2, 3)
-HNY_Column <- "Trust"
 HNY_Value <- c("Hull University Teaching Hospitals NHS Trust",
                "York and Scarborough Teaching Hospitals NHS Foundation Trust",
                "Northern Lincolnshire and Goole NHS Foundation Trust")
 
-HNY_df[HNY_Row, HNY_Column] <- HNY_Value
+HNY_Org <- c("RWA",
+             "RCB",
+             "RJL")
 
-# Cleaning North East London
+HNY_df <- HNY_df %>% 
+  mutate(`Org code` = HNY_Org,
+         Trust = ifelse(row_number() == HNY_Row, HNY_Value, Trust))
+
+# Cleaning NEL1
 
 remove_NELCA <- 4
-NEL1_df <- NEL1_df[-remove_NELCA, ]
+
+NEL1_df <- NEL1_df %>%
+  slice(-remove_NELCA)
 
 # Cleaning NWL1
 
-remove_NWL1 <- c(3, 5, 6, 7)
-NWL1_df <- NWL1_df[remove_NWL1, ]
+remove_NWL1 <- c(1, 2, 4)
+
+NWL1_Org <- c("R1K",
+              "RAS",
+              "RQM",
+              "RYJ")
+
+NWL1_df <- NWL1_df %>%
+  slice(-remove_NWL1) %>%
+  mutate(`Org code` = NWL1_Org)
 
 # Cleaning SE2
 
@@ -98,12 +158,21 @@ SE2_Dupli <- 5
 SE2_df <- rbind(SE2_df, SE2_df[SE2_Dupli, ], SE2_df[SE2_Dupli, ])
 
 SE2_Row <- c(5, 6, 7)
-SE2_Column <- "Trust"
 SE2_Value <- c("University Hospitals Sussex NHS Foundation Trust",
                "Queen Victoria Hospital NHS Foundation Trust",
                "Surrey & Sussex Healthcare NHS Trust")
 
-SE2_df[SE2_Row, SE2_Column] <- SE2_Value
+SE2_Org <- c("RDU",
+             "RTK",
+             "RXC",
+             "RA2",
+             "RYR",
+             "RPC",
+             "RTP")
+
+SE2_df <- SE2_df %>% 
+  mutate(`Org code` = SE2_Org,
+         Trust = ifelse(row_number() == SE2_Row, SE2_Value, Trust))
 
 # Combining all cleaned df
 
