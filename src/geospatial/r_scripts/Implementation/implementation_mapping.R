f4205b86e76b968462ac6fd98f2144fd093c18f1
Elliot Royle <87072751+elliotroyle@users.noreply.github.com>
2024-04-16 16:02:57 +0100

    Changes to formatting for .Rmd output.

--- src/geospatial/r_scripts/Implementation/implementation_mapping.R
+++ src/geospatial/r_scripts/Implementation/implementation_mapping.R
@@ -1,40 +1,38 @@
 
 # Implementation Mapping Outputs
 
-## AIDF data load
-
-AIDF_Geo_Final <- read.csv(paste0(here(),"/data/processed SFBC/AIDF_Geo_Final.csv"))
-
-AIDF_Geo_Final <- AIDF_Geo_Final %>%
+# Load AIDF data
+AIDF_Geo_Final <- read.csv(paste0(here(),"/data/processed SFBC/AIDF_Geo_Final.csv")) %>%
   rename("Network Name 1" = Network.Name.1,
          "Network Name 2" = Network.Name.2,
-         "Org code" = Org.code,
-         )
+         "Org code" = Org.code)
 
-## Merging the constituent data into one source
-
+# Load OHID lookup data
 OHID_Lookup_Path <- (paste0(here(), "/data/geospatial/trust_msoa_lookup.xlsx"))
 OHID_Lookup <- read_xlsx(OHID_Lookup_Path)
 
+# Merge AIDF and OHID data
 AIDF_Geo_Join <- left_join(OHID_Lookup, AIDF_Geo_Final, by = c("TrustCode" = "Org code"))
 
+# Add status column
 AIDF_Geo_LNl <- AIDF_Geo_Join %>%
   mutate(Status = ifelse(!is.na(SFBC_ID), "Live", "Not Live"))
 
-## Merging the final AIFG_Geo df and OHID shape file
-
+# Merge final data with OHID shape file
 OHID_merge <- left_join(OHID_msoa_shp, AIDF_Geo_LNl, by = c("TrustCd" = "TrustCode"))
 
-## Filtering the shape files based on the value in `Status`
-
+# Filter shape files based on Status
 OHID_shp_NL <- OHID_merge %>%
   filter(Status == "Not Live")
 
 OHID_shp_L <- OHID_merge %>%
   filter(Status == "Live")
 
-## Formatting the date fields correctly for use in R shiny app
-
+# Format date fields
 OHID_shp_dates <- OHID_shp_L %>%
   mutate(Net_Implementation_Start = as.Date(Net_Implementation_Start, format = "%d/%m/%Y"),
          Net_Implementation_End = as.Date(Net_Implementation_End, format = "%d/%m/%Y"))
+
+color_palette <- colorFactor(
+  palette = c("#b15928", "#34baeb", "#262626", "#33a02c", "#cab2d6", "#e31a1c", "#fdbf6f", "#ff7f00", "#6a3d9a", "#fb9a99", "#c51b7d", "#3458eb"),
+  domain = unique(OHID_shp_dates$`Network Name 1`))
